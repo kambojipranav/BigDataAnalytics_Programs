3b0dbf55072ab282d4d64c19a1d9ed4eb0a81fc5
Kamboji Pranav <kambojipranav@gmail.com>
2025-11-05 16:08:10 +0530

    Update reducer.py

--- WordCount/reducer.py
+++ WordCount/reducer.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
+import sys
 
+word_count = {}
+
+for line in sys.stdin:
+    word, count = line.strip().split("\t")
+    count = int(count)
+    word_count[word] = word_count.get(word, 0) + count
+
+for word in word_count:
+    print "%s\t%d" % (word, word_count[word])
